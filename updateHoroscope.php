37f1b93760a130603e22a0751fe05f9771f39b13
sjogrenjohan <sjogren_johan@hotmail.com>
2019-01-18 14:42:45 +0100

    no worky

--- updateHoroscope.php
+++ updateHoroscope.php
@@ -1,15 +1,17 @@
 <?php
     parse_str(file_get_contents("php://input"), $_PUT);
     session_start();
+    require "./horoscopeGenerator.php";
 
-    require "./addHoroscope.php";
-
-    if($_SERVER['REQUEST_METHOD'] == 'PUT'){
-        
-        if(isset($_SESSION["horoskop"])) {
-            session_unset();
-            $_SESSION["horoskop"] = $personNummer;
+    if($_SERVER['REQUEST_METHOD'] == 'PUT' && isset($_PUT)) {
+        if(isset($_SESSION["horoskop"]) && isset($_PUT["personNumber"])) {
+            updateHoroscope($_PUT["personNumber"], true);
+            echo json_encode(array("success"=>true));
+        } else {
+            echo json_encode(false);
         }
+    } else {
+        echo json_encode(array('success'=>false));
     }
 
 ?>
